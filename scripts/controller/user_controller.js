3b123c1c443789f325fedbaf2f91f31c723f4cfa
AninaRJ <ranjani.shivashankar@gmail.com>
2016-04-01 17:50:44 +0530

    Committing new changes
    Changes are being made to bring in a new export file system

--- scripts/controller/user_controller.js
+++ scripts/controller/user_controller.js
@@ -4,4 +4,14 @@ edusiteApp.controller('UserController', function($scope){
 	$scope.submitform = function(){
 		console.log("submitted form");
 	};
+	
+	$scope.uploadFile = function(){
+               var file = $scope.myFile;
+               
+               console.log('file is ' );
+               console.dir(file);
+               
+               var uploadUrl = "/fileUpload";
+               fileUpload.uploadFileToUrl(file, uploadUrl);
+            };
 });
--- scripts/edusite.js
+++ scripts/edusite.js
@@ -6,3 +6,19 @@ var edusiteApp = angular.module("edusiteApp", ["ngMaterial"]).config(function($m
 	  .accentPalette('light-blue')
 	  .backgroundPalette('brown');
 	});
+	
+edusiteApp.directive('fileModel', ['$parse', function ($parse) {
+            return {
+               restrict: 'A',
+               link: function(scope, element, attrs) {
+                  var model = $parse(attrs.fileModel);
+                  var modelSetter = model.assign;
+                  
+                  element.bind('change', function(){
+                     scope.$apply(function(){
+                        modelSetter(scope, element[0].files[0]);
+                     });
+                  });
+               }
+            };
+         }]);
